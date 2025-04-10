ce8254afcffc5b76a91a2331c909847e9a17d1f3
Pratik Chavan <pratik.logicloop@gmail.com>
2025-04-10 23:39:32 +0530

    some more animation

--- components/ProjectDetail/projectOverview.jsx
+++ components/ProjectDetail/projectOverview.jsx
@@ -2,7 +2,8 @@ import BlackButton from "@/common/BlackButton";
 import SectionTitle from "@/common/SectionTitle";
 import Image from "next/image";
 import React from "react";
-
+import { slideIn } from "@/Animation/Variants";
+import { motion } from "framer-motion";
 const ProjectOverview = () => {
   return (
     <div
@@ -18,34 +19,51 @@ const ProjectOverview = () => {
       </div>
       <div className="">
         <div className="flex justify-between lg:flex-row flex-col items-center">
-          <div className="lg:h-[35.104vw] h-full lg:w-[28.542vw] w-full">
-            <Image
-              src={"/images/project-details/projectOverview.png"}
-              width={548}
-              height={674}
-              className="h-full w-full object-cover"
-            />
+          <div className="overflow-x-hidden">
+            <motion.div
+              initial="hidden"
+              whileInView="show"
+              variants={slideIn("right", 0.3)}
+            >
+              <div className="lg:h-[35.104vw] h-full lg:w-[28.542vw] w-full">
+                <Image
+                  src={"/images/project-details/projectOverview.png"}
+                  width={548}
+                  height={674}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </motion.div>
           </div>
           <div className=" lg:w-1/2 flex flex-col lg:gap-[2.083vw] gap-4 lg:mb-0 ">
             <div className="mt-4">
               <SectionTitle title="OVERVIEW" />
             </div>
-            <p className="lg:text-[1.042vw] text-base lg:leading-[1.667vw] leading-5">
-              Lorem ipsum dolor sit amet consectetur. Et bibendum diam volutpat
-              nibh sagittis orci dolor. Ipsum amet et elementum ipsum.
-              Condimentum faucibus lacinia tempus vulputate sit arcu egestas.
-              Lorem ipsum dolor sit amet consectetur. Et bibendum diam volutpat
-              nibh sagittis orci dolor. Ipsum amet et elementum ipsum.
-              Condimentum faucibus lacinia tempus vulputate sit arcu egestas.
-            </p>
-            <BlackButton
-              name="Download Brochure"
-              path="/learn"
-              color="#000000"
-              hoverColor="#C29B5C"
-              textColor="#ffffff"
-              hoverTextColor="#000000"
-            />
+            <div>
+              <motion.div
+                initial="hidden"
+                whileInView="show"
+                variants={slideIn("left", 0.3)}
+              >
+                <p className="lg:text-[1.042vw] text-base lg:leading-[1.667vw] leading-5">
+                  Lorem ipsum dolor sit amet consectetur. Et bibendum diam
+                  volutpat nibh sagittis orci dolor. Ipsum amet et elementum
+                  ipsum. Condimentum faucibus lacinia tempus vulputate sit arcu
+                  egestas. Lorem ipsum dolor sit amet consectetur. Et bibendum
+                  diam volutpat nibh sagittis orci dolor. Ipsum amet et
+                  elementum ipsum. Condimentum faucibus lacinia tempus vulputate
+                  sit arcu egestas.
+                </p>
+                <BlackButton
+                  name="Download Brochure"
+                  path="/learn"
+                  color="#000000"
+                  hoverColor="#C29B5C"
+                  textColor="#ffffff"
+                  hoverTextColor="#000000"
+                />
+              </motion.div>
+            </div>
           </div>
         </div>
       </div>
